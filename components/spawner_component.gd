eb5f9f35cf6071ab837947c3fffdb4bf2beef352
Carsten <cuser@gmx.de>
2024-12-25 10:47:44 +0100

    Flame spawn in the right direction

--- components/spawner_component.gd
+++ components/spawner_component.gd
@@ -12,12 +12,19 @@ extends Node3D
 func spawn(global_spawn_position: Vector3 = global_position, parent: Node = get_tree().current_scene) -> Node:
 	assert(scene is PackedScene, "Error: The scene export was never set on this spawner component.")
 	# Instance the scene
-	var instance: Node = scene.instantiate()
+	var instance: Node3D = scene.instantiate()
 	# Add it as a child of the parent
 	parent.add_child(instance)
 	# Update the global position of the instance.
 	# (This must be done after adding it as a child)
 	instance.global_position = global_spawn_position
+	print(self.global_rotation)
+	if(instance.has_method("set_direction") && instance.has_method("get_direction")):
+		var current_direction:Vector3 = instance.get_direction()
+		var new_rotation:float = self.global_rotation.y
+		var new_direction:Vector3 = current_direction.rotated(Vector3.UP, new_rotation)
+		instance.set_direction(new_direction)
+		
 	# Return the instance in case we want to perform any other operations
 	# on it after instancing it.
 	return instance
